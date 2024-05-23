2f555241db382b3595261212719ecf6e1308e126
olivierdelobre <olivier.delobre@gmail.com>
2024-05-23 14:53:09 +0200

    add function to get http status category label

--- api/utils.go
+++ api/utils.go
@@ -3,6 +3,9 @@ package api
 import api "github.com/epfl-si/go-toolbox/api/models"
 
 func MakeError(errorType string, status int, message string, detail string, instance string, help string) *api.Error {
+	if errorType == "" {
+		errorType = GetHttpStatusCategory(status)
+	}
 	return &api.Error{
 		Type:     errorType,
 		Title:    message,
@@ -10,5 +13,22 @@ func MakeError(errorType string, status int, message string, detail string, inst
 		Detail:   detail,
 		Instance: instance,
 		Help:     help,
+	}
+}
+
+func GetHttpStatusCategory(status int) string {
+	switch {
+	case status >= 100 && status < 200:
+		return "information"
+	case status >= 200 && status < 300:
+		return "success"
+	case status >= 300 && status < 400:
+		return "redirection"
+	case status >= 400 && status < 500:
+		return "client error"
+	case status >= 500 && status < 600:
+		return "server error"
+	default:
+		return "unknown"
 	}
 }
