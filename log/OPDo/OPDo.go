84d92c8985cf16ea05942bb9c109f1e81182e24d
Arnaud (Arhuman) ASSAD <arhuman@gmail.com>
2025-03-17 14:22:30 +0100

    add logfile configuration through env var, and technical logging

--- log/OPDo/OPDo.go
+++ log/OPDo/OPDo.go
@@ -1,3 +1,4 @@
+// Package opdo handles OPDo logging
 package opdo
 
 import (
@@ -7,12 +8,20 @@ import (
 	"os"
 )
 
-// LogOPDo(handler_id, handled_id, crudt, source, payload)
+var logFile = "/logs/OPDo.log"
+
+func init() {
+	// Check if the env variable OPDO_LOGFILE exists, and if it does, set logFile to the value of the env variable
+	if os.Getenv("OPDO_LOGFILE") != "" {
+		logFile = os.Getenv("OPDO_LOGFILE")
+	}
+}
+
 // LogOPDo logs the operation of the user identified by handler_id on the data of the user identified by handled_id in the file /logs/OPDo.log
 // Parameters:
 // - log: a log.Logger object
-// - handler_id: the id of the user who performed the operation
-// - handled_id: the id of the user whose data was operated on
+// - handlerID: the id of the user who performed the operation
+// - handledID: the id of the user whose data was operated on
 // - crudt: the type of operation performed
 //   - C - Creation
 //   - R - Read access
@@ -22,27 +31,39 @@ import (
 //
 // - source: the source of the operation (e.g. the name of the application/IP address)
 // - payload: DPO readable description of the access
-// TODO: Should maybe take a logger parameter to log errors ?
-func LogOPDo(log *log.Logger, handler_id string, handled_id string, crudt string, source string, payload string) {
-
-	// Open the file for writing
-	file, err := os.OpenFile("/logs/OPDo.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Printf("failed creating file: %s", err)
+// - caller: caller information, used to produced technical logging if not empty
+func LogOPDo(log *log.Logger, handlerID string, handledID string, crudt string, source string, payload string, caller string) {
+	// Technical logging
+	if caller != "" {
+		log.Printf("OPDo: In %s (%s), %s accessed %s data in %s mode: %s", caller, source, handlerID, handledID, crudt, payload)
 	}
 
-	// Defer closing the file
-	defer file.Close()
+	// Open the file for writing
+	file, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("failed creating file: %s", err)
+		return
+	}
+	// Ensure the file is closed even if an error occurs
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Printf("failed closing file: %s", err)
+		}
+	}()
 
 	// Create a new writer
 	writer := bufio.NewWriter(file)
 
 	// Write the log
-	_, err = fmt.Fprintf(writer, "\"%s\";\"%s\";\"%s\";\"%s\";\"%s\"\n", handler_id, handled_id, crudt, source, payload)
+	_, err = fmt.Fprintf(writer, "\"%s\";\"%s\";\"%s\";\"%s\";\"%s\"\n", handlerID, handledID, crudt, source, payload)
 	if err != nil {
 		log.Printf("failed writing to file: %s", err)
+		return
 	}
 
 	// Flush the writer
-	writer.Flush()
+	if err := writer.Flush(); err != nil {
+		log.Printf("failed flushing writer: %s", err)
+		return
+	}
 }
