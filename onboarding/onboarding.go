0f8b5e6b5bb6ce420303517b1c9c40f8e4af81c1
Adrien <88890846+origawi@users.noreply.github.com>
2025-04-22 10:08:59 +0200

    add onboarding

---
+++ onboarding/onboarding.go
@@ -0,0 +1,34 @@
+package onboarding
+
+const (
+	STAFF                     = "Staff"
+	STUDENT                   = "Student"
+	EXTERNAL                  = "External"
+	SERVICE                   = "Service"
+	AUDIT                     = "AUDIT"
+	AUDIT_EDOC                = "AUDIT_EDOC"
+	EFC                       = "EFC"
+	EDOC                      = "EDOC"
+	CDOC                      = "CDOC"
+	BAMA                      = "BAMA"
+	BAMA_MOBILITE             = "BAMA_MOBILITE"
+	BAMA_MOBILITE_AVEC_PROJET = "BAMA_MOBILITE_AVEC_PROJET"
+	BAMA_MOBILITE_SANS_PROJET = "BAMA_MOBILITE_SANS_PROJET"
+	BAMA_MOBILITE_MNIS_SUISSE = "BAMA_MOBILITE_MNIS_SUISSE"
+)
+
+var AllowedStudentTypes = map[string]bool{
+	STAFF:                     true,
+	STUDENT:                   true,
+	EXTERNAL:                  true,
+	AUDIT:                     true,
+	AUDIT_EDOC:                true,
+	EFC:                       true,
+	EDOC:                      true,
+	CDOC:                      true,
+	BAMA:                      true,
+	BAMA_MOBILITE:             true,
+	BAMA_MOBILITE_AVEC_PROJET: true,
+	BAMA_MOBILITE_SANS_PROJET: true,
+	BAMA_MOBILITE_MNIS_SUISSE: true,
+}
