9c7a113a3b1ee1202476fd3748c1d0dfc59f1cf5
Adrien <88890846+origawi@users.noreply.github.com>
2025-06-19 14:36:02 +0200

    add IsaProfs, replace Sciper with Persid

---
+++ database/models/IsaProfs.go
@@ -0,0 +1,14 @@
+package database
+
+import "time"
+
+type IsaProfs struct {
+	Persid    string    `gorm:"column:sciper"`
+	Status    string    `gorm:"column:statut"`
+	StartDate time.Time `gorm:"column:datedeb"`
+	EndDate   time.Time `gorm:"column:datefin"`
+}
+
+func (balise *IsaProfs) TableName() string {
+	return "dinfo.isa_profs"
+}
