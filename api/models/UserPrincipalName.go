594af5f891783a3bc94a55b80338fb8818dfe051
Adrien <88890846+origawi@users.noreply.github.com>
2025-07-14 15:56:44 +0200

    add UPN

---
+++ api/models/UserPrincipalName.go
@@ -0,0 +1,11 @@
+package api
+
+import "time"
+
+type UserPrincipalName struct {
+	PersId    string    `json:"persid"`
+	UPN       string    `json:"upn"`
+	SynchroAD bool      `json:"synchro_ad"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
