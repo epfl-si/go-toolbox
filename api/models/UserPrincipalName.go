d8994dc0fe8a93442ee89b3eccfec0c13169fe1b
Adrien <88890846+origawi@users.noreply.github.com>
2025-07-16 11:28:31 +0200

    add type to UPN json

--- api/models/UserPrincipalName.go
+++ api/models/UserPrincipalName.go
@@ -5,6 +5,7 @@ import "time"
 type UserPrincipalName struct {
 	PersId    string    `json:"persid"`
 	UPN       string    `json:"upn"`
+	Type      string    `json:"type"`
 	SynchroAD bool      `json:"synchro_ad"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
