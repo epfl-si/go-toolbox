3d81a1c6e726cc6ef773524f8832b8f945f1b173
Adrien <88890846+origawi@users.noreply.github.com>
2025-09-16 10:11:43 +0200

    update json output or PrivateEmail

--- api/models/PrivateEmail.go
+++ api/models/PrivateEmail.go
@@ -1,8 +1,11 @@
 package api
 
+import "time"
+
 type PrivateEmail struct {
-	PersId string `json:"persid"`
-	Email  string `json:"email"`
-	Status bool   `json:"status"`
-	Source string `json:"source"`
+	PersId    string    `json:"persid"`
+	Email     string    `json:"email"`
+	Status    bool      `json:"status"`
+	Source    string    `json:"source"`
+	CreatedAt time.Time `json:"created_at"`
 }
