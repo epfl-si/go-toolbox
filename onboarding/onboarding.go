9d383b288748cc75d6171071152902d899b0a7ba
Adrien <88890846+origawi@users.noreply.github.com>
2025-12-11 16:57:21 +0100

    add MAX_ONBOARDING_* dates and misc

--- onboarding/onboarding.go
+++ onboarding/onboarding.go
@@ -1,20 +1,27 @@
 package onboarding
 
+import "time"
+
 const (
-	STAFF                     = "Staff"
-	STUDENT                   = "Student"
-	EXTERNAL                  = "External"
-	SERVICE                   = "Service"
 	AUDIT                     = "AUDIT"
 	AUDIT_EDOC                = "AUDIT_EDOC"
-	EFC                       = "EFC"
-	EDOC                      = "EDOC"
-	CDOC                      = "CDOC"
 	BAMA                      = "BAMA"
 	BAMA_MOBILITE             = "BAMA_MOBILITE"
 	BAMA_MOBILITE_AVEC_PROJET = "BAMA_MOBILITE_AVEC_PROJET"
-	BAMA_MOBILITE_SANS_PROJET = "BAMA_MOBILITE_SANS_PROJET"
 	BAMA_MOBILITE_MNIS_SUISSE = "BAMA_MOBILITE_MNIS_SUISSE"
+	BAMA_MOBILITE_SANS_PROJET = "BAMA_MOBILITE_SANS_PROJET"
+	CDOC                      = "CDOC"
+	EDOC                      = "EDOC"
+	EFC                       = "EFC"
+	EXTERNAL                  = "External"
+	FBM                       = "FBM"
+	MAX_ONBOARDING_FOR_EDOC   = -61 * 24 * time.Hour // 2 months
+	MAX_ONBOARDING_FOR_EFC    = -15 * 24 * time.Hour // 15 days
+	MAX_ONBOARDING_FOR_STAFF  = -3 * 24 * time.Hour  // 3 days
+	MAX_ONBOARDING_PERIOD     = -91 * 24 * time.Hour // For anyone : max onboarding 3 months before
+	SERVICE                   = "Service"
+	STAFF                     = "Staff"
+	STUDENT                   = "Student"
 )
 
 var AllowedStudentTypes = map[string]bool{
